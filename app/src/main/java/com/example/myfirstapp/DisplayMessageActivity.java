1ea403d726408bbd1f77467c88bbb7a1924eaeb1
manghuang <1255529524@qq.com>
2021-03-31 22:07:53 +0800

    完成了使用自带的相机Activity拍照和录像

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,12 +1,22 @@
 package com.example.myfirstapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Camera;
+import android.hardware.camera2.*;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -15,15 +25,12 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
     }
 
     // 控制相机
     private Camera camera;
+    private Preview preview;
+
     private boolean safeCameraOpen(int id) {
         boolean qOpened = false;
 
@@ -46,5 +53,87 @@ public class DisplayMessageActivity extends AppCompatActivity {
             camera = null;
         }
     }
+
+    class Preview extends ViewGroup implements SurfaceHolder.Callback {
+
+        SurfaceView surfaceView;
+        SurfaceHolder holder;
+        Camera mCamera;
+        private List<Camera.Size> supportedPreviewSizes;
+
+        Preview(Context context) {
+            super(context);
+
+            surfaceView = new SurfaceView(context);
+            addView(surfaceView);
+
+            // Install a SurfaceHolder.Callback so we get notified when the
+            // underlying surface is created and destroyed.
+            holder = surfaceView.getHolder();
+            holder.addCallback(this);
+            holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        @Override
+        protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+        }
+
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+            // Now that the size is known, set up the camera parameters and begin
+            // the preview.
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(previewSize.width, previewSize.height);
+            requestLayout();
+            mCamera.setParameters(parameters);
+
+            // Important: Call startPreview() to start updating the preview surface.
+            // Preview must be started before you can take a picture.
+            mCamera.startPreview();
+        }
+
+
+        // 重启预览
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+        }
+
+        public void setCamera(Camera camera) {
+            if (mCamera == camera) { return; }
+
+            stopPreviewAndFreeCamera();
+
+            mCamera = camera;
+
+            if (mCamera != null) {
+                List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+                supportedPreviewSizes = localSizes;
+                requestLayout();
+
+                try {
+                    mCamera.setPreviewDisplay(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Important: Call startPreview() to start updating the preview
+                // surface. Preview must be started before you can take a picture.
+                mCamera.startPreview();
+            }
+        }
+
+        private void stopPreviewAndFreeCamera() {
+        }
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.FileProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -23,6 +24,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
 
+    // 生命周期第一步
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,52 +32,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // 发送消息
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
-    public void sendMessage(View view){
+    // 自定义相机
+    public void selfCamera(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
 
-
-
     // 获得图片
-    public void getImage(View view){
+    private String currentPhotoPath;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
+
+    public void getImage(View view) {
         System.out.println("获得图片");
         dispatchTakePictureIntent();
         galleryAddPic();
     }
 
-//    static final int REQUEST_IMAGE_CAPTURE = 1;
-//
-//    private void dispatchTakePictureIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//        }
-//    }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            System.out.println("成功返回");
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            ImageView imageView = findViewById(R.id.imageView);
-//            imageView.setImageBitmap(imageBitmap);
-//        }
-//    }
-
-    String currentPhotoPath;
-
-    static final int REQUEST_TAKE_PHOTO = 1;
-
+    // 创建保存照片的文件
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -93,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+
+    // 保存完整尺寸的照片
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -108,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
             // Continue only if the File was successfully created
             if (photoFile != null) {
                 Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
+                        "com.example.myfirstapp.fileprovider", // 必须保持一致，而且需要是包名
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
@@ -116,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //将照片添加到图库
     private void galleryAddPic() {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(currentPhotoPath);
@@ -125,16 +103,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // 展示
+    private void setPic() {
+        ImageView imageView = findViewById(R.id.imageView);
+
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+        System.out.println("W；" + targetW + "  H:" + targetH);
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        System.out.println(bitmap);
+        imageView.setImageBitmap(bitmap);
+    }
+
 
     // 获得视频
-    public void getVideo(View view){
+    static final int REQUEST_VIDEO_CAPTURE = 2;
+
+    public void getVideo(View view) {
         System.out.println("获得视频");
         dispatchTakeVideoIntent();
     }
 
 
-    static final int REQUEST_VIDEO_CAPTURE = 1;
-
+    // 拍摄
     private void dispatchTakeVideoIntent() {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
@@ -142,17 +149,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // 回调展示
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            System.out.println("视频成功返回");
             Uri videoUri = intent.getData();
+//            System.out.println(videoUri);
             VideoView videoView = findViewById(R.id.videoView);
+//            System.out.println(videoView);
             videoView.setVideoURI(videoUri);
+            videoView.start();
+        } else if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            System.out.println("拍照成功返回");
+            setPic();
         }
     }
-
-
 
 
 }
